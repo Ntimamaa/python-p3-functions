928e4c2df755a4023e8031a59208feed3c3aa64a
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-08-16 15:05:56 +0300

    Completed Assingment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!") 
+
+greet('Naureen')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")  
+
+greet_with_default()
+greet_with_default('Sunny')
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2 
+    return sum  
+
+result = add(45, 55)
+print(result)
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    else:
+        return number / 2 
